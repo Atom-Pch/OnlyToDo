6744739d459b76043ee087ab0db95e59bcb33641
AtomP <112673913+Atom-Pch@users.noreply.github.com>
2026-05-27 23:07:49 +0700

    removed prom and graf repo tf outputs

--- iac/terraform/ecr/outputs.tf
+++ iac/terraform/ecr/outputs.tf
@@ -4,22 +4,10 @@ output "frontend_repo_name" {
 output "backend_repo_name" {
   value = module.backend_repo.repository_name
 }
-output "prom_repo_name" {
-  value = module.prom_repo.repository_name
-}
-output "graf_repo_name" {
-  value = module.graf_repo.repository_name
-}
 
 output "frontend_repo_url" {
   value = module.frontend_repo.repository_url
 }
 output "backend_repo_url" {
   value = module.backend_repo.repository_url
-}
-output "prom_repo_url" {
-  value = module.prom_repo.repository_url
-}
-output "graf_repo_url" {
-  value = module.graf_repo.repository_url
 }
