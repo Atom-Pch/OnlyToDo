b273000b60876e61956240e60148898e05323327
AtomP <112673913+Atom-Pch@users.noreply.github.com>
2026-05-30 00:51:58 +0700

    fixed ns backend metrics issue

--- backend/middleware.go
+++ backend/middleware.go
@@ -44,7 +44,7 @@ var (
 	dbQueryDuration = promauto.NewHistogramVec(prometheus.HistogramOpts{
 		Name:    "db_query_duration_seconds",
 		Help:    "Duration of database queries.",
-		Buckets: []float64{0.001, 0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1},
+		Buckets: []float64{0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1},
 	}, []string{"operation"})
 )
 
