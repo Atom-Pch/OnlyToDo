ff9099fe8718262f43a54ba9e56233120d1a2c35
AtomP <112673913+Atom-Pch@users.noreply.github.com>
2026-05-30 19:20:34 +0700

    bring back vpc endpoint

--- iac/terraform/vpc/main.tf
+++ iac/terraform/vpc/main.tf
@@ -42,20 +42,20 @@ module "vpc" {
 }
 
 # SECURITY GROUP
-# module "vpce_sg" {
-#   source  = "terraform-aws-modules/security-group/aws"
-#   version = ">= 5.3.1"
+module "vpce_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = ">= 5.3.1"
 
-#   name        = "vpce-endpoints-sg"
-#   description = "Allow connections for VPC endpoint"
-#   vpc_id      = module.vpc.vpc_id
+  name        = "vpce-endpoints-sg"
+  description = "Allow connections for VPC endpoint"
+  vpc_id      = module.vpc.vpc_id
 
-#   ingress_rules       = ["https-443-tcp"]
-#   ingress_cidr_blocks = [var.vpc_cidr]
+  ingress_rules       = ["https-443-tcp"]
+  ingress_cidr_blocks = [var.vpc_cidr]
 
-#   egress_rules       = ["all-tcp"]
-#   egress_cidr_blocks = ["0.0.0.0/0"]
-# }
+  egress_rules       = ["all-tcp"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
 
 # NAT GATEWAY (fck-nat)
 module "fck_nat" {
@@ -87,127 +87,127 @@ resource "aws_vpc_endpoint" "s3" {
   tags = { Name = "todo-vpce-s3" }
 }
 
-# resource "aws_vpc_endpoint" "ecr_dkr" {
-#   vpc_id             = module.vpc.vpc_id
-#   service_name       = "com.amazonaws.${var.aws_region}.ecr.dkr"
-#   vpc_endpoint_type  = "Interface"
-#   subnet_ids         = module.vpc.private_subnets
-#   security_group_ids = [module.vpce_sg.security_group_id]
+resource "aws_vpc_endpoint" "ecr_dkr" {
+  vpc_id             = module.vpc.vpc_id
+  service_name       = "com.amazonaws.${var.aws_region}.ecr.dkr"
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = module.vpc.private_subnets
+  security_group_ids = [module.vpce_sg.security_group_id]
 
-#   private_dns_enabled = true
+  private_dns_enabled = true
 
-#   tags = { Name = "todo-vpce-ecr.dkr" }
-# }
+  tags = { Name = "todo-vpce-ecr.dkr" }
+}
 
-# resource "aws_vpc_endpoint" "ecr_api" {
-#   vpc_id             = module.vpc.vpc_id
-#   service_name       = "com.amazonaws.${var.aws_region}.ecr.api"
-#   vpc_endpoint_type  = "Interface"
-#   subnet_ids         = module.vpc.private_subnets
-#   security_group_ids = [module.vpce_sg.security_group_id]
+resource "aws_vpc_endpoint" "ecr_api" {
+  vpc_id             = module.vpc.vpc_id
+  service_name       = "com.amazonaws.${var.aws_region}.ecr.api"
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = module.vpc.private_subnets
+  security_group_ids = [module.vpce_sg.security_group_id]
 
-#   private_dns_enabled = true
+  private_dns_enabled = true
 
-#   tags = { Name = "todo-vpce-ecr.api" }
-# }
+  tags = { Name = "todo-vpce-ecr.api" }
+}
 
-# resource "aws_vpc_endpoint" "logs" {
-#   vpc_id             = module.vpc.vpc_id
-#   service_name       = "com.amazonaws.${var.aws_region}.logs"
-#   vpc_endpoint_type  = "Interface"
-#   subnet_ids         = module.vpc.private_subnets
-#   security_group_ids = [module.vpce_sg.security_group_id]
+resource "aws_vpc_endpoint" "logs" {
+  vpc_id             = module.vpc.vpc_id
+  service_name       = "com.amazonaws.${var.aws_region}.logs"
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = module.vpc.private_subnets
+  security_group_ids = [module.vpce_sg.security_group_id]
 
-#   private_dns_enabled = true
+  private_dns_enabled = true
 
-#   tags = { Name = "todo-vpce-cloudwatch-logs" }
-# }
+  tags = { Name = "todo-vpce-cloudwatch-logs" }
+}
 
-# resource "aws_vpc_endpoint" "secret" {
-#   vpc_id             = module.vpc.vpc_id
-#   service_name       = "com.amazonaws.${var.aws_region}.secretsmanager"
-#   vpc_endpoint_type  = "Interface"
-#   subnet_ids         = module.vpc.private_subnets
-#   security_group_ids = [module.vpce_sg.security_group_id]
+resource "aws_vpc_endpoint" "secret" {
+  vpc_id             = module.vpc.vpc_id
+  service_name       = "com.amazonaws.${var.aws_region}.secretsmanager"
+  vpc_endpoint_type  = "Interface"
+  subnet_ids         = module.vpc.private_subnets
+  security_group_ids = [module.vpce_sg.security_group_id]
 
-#   private_dns_enabled = true
+  private_dns_enabled = true
 
-#   tags = { Name = "todo-vpce-secret-manager" }
-# }
+  tags = { Name = "todo-vpce-secret-manager" }
+}
 
-# resource "aws_vpc_endpoint" "sts" {
-#   vpc_id              = module.vpc.vpc_id
-#   service_name        = "com.amazonaws.${var.aws_region}.sts"
-#   vpc_endpoint_type   = "Interface"
-#   private_dns_enabled = true
-#   subnet_ids          = module.vpc.private_subnets
-#   security_group_ids  = [module.vpce_sg.security_group_id]
+resource "aws_vpc_endpoint" "sts" {
+  vpc_id              = module.vpc.vpc_id
+  service_name        = "com.amazonaws.${var.aws_region}.sts"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  subnet_ids          = module.vpc.private_subnets
+  security_group_ids  = [module.vpce_sg.security_group_id]
 
-#   tags = { Name = "todo-vpce-sts" }
-# }
+  tags = { Name = "todo-vpce-sts" }
+}
 
-# resource "aws_vpc_endpoint" "ec2" {
-#   vpc_id              = module.vpc.vpc_id
-#   service_name        = "com.amazonaws.${var.aws_region}.ec2"
-#   vpc_endpoint_type   = "Interface"
-#   private_dns_enabled = true
-#   subnet_ids          = module.vpc.private_subnets
-#   security_group_ids  = [module.vpce_sg.security_group_id]
+resource "aws_vpc_endpoint" "ec2" {
+  vpc_id              = module.vpc.vpc_id
+  service_name        = "com.amazonaws.${var.aws_region}.ec2"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  subnet_ids          = module.vpc.private_subnets
+  security_group_ids  = [module.vpce_sg.security_group_id]
 
-#   tags = { Name = "todo-vpce-ec2" }
-# }
+  tags = { Name = "todo-vpce-ec2" }
+}
 
-# resource "aws_vpc_endpoint" "eks" {
-#   vpc_id              = module.vpc.vpc_id
-#   service_name        = "com.amazonaws.${var.aws_region}.eks"
-#   vpc_endpoint_type   = "Interface"
-#   private_dns_enabled = true
-#   subnet_ids          = module.vpc.private_subnets
-#   security_group_ids  = [module.vpce_sg.security_group_id]
+resource "aws_vpc_endpoint" "eks" {
+  vpc_id              = module.vpc.vpc_id
+  service_name        = "com.amazonaws.${var.aws_region}.eks"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  subnet_ids          = module.vpc.private_subnets
+  security_group_ids  = [module.vpce_sg.security_group_id]
 
-#   tags = { Name = "todo-vpce-eks" }
-# }
+  tags = { Name = "todo-vpce-eks" }
+}
 
-# resource "aws_vpc_endpoint" "autoscaling" {
-#   vpc_id              = module.vpc.vpc_id
-#   service_name        = "com.amazonaws.${var.aws_region}.autoscaling"
-#   vpc_endpoint_type   = "Interface"
-#   private_dns_enabled = true
-#   subnet_ids          = module.vpc.private_subnets
-#   security_group_ids  = [module.vpce_sg.security_group_id]
+resource "aws_vpc_endpoint" "autoscaling" {
+  vpc_id              = module.vpc.vpc_id
+  service_name        = "com.amazonaws.${var.aws_region}.autoscaling"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  subnet_ids          = module.vpc.private_subnets
+  security_group_ids  = [module.vpce_sg.security_group_id]
 
-#   tags = { Name = "todo-vpce-autoscaling" }
-# }
+  tags = { Name = "todo-vpce-autoscaling" }
+}
 
-# resource "aws_vpc_endpoint" "eks_auth" {
-#   vpc_id              = module.vpc.vpc_id
-#   service_name        = "com.amazonaws.${var.aws_region}.eks-auth"
-#   vpc_endpoint_type   = "Interface"
-#   private_dns_enabled = true
-#   subnet_ids          = module.vpc.private_subnets
-#   security_group_ids  = [module.vpce_sg.security_group_id]
+resource "aws_vpc_endpoint" "eks_auth" {
+  vpc_id              = module.vpc.vpc_id
+  service_name        = "com.amazonaws.${var.aws_region}.eks-auth"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  subnet_ids          = module.vpc.private_subnets
+  security_group_ids  = [module.vpce_sg.security_group_id]
 
-#   tags = { Name = "todo-vpce-eks-auth" }
-# }
+  tags = { Name = "todo-vpce-eks-auth" }
+}
 
-# resource "aws_vpc_endpoint" "elasticloadbalancing" {
-#   vpc_id              = module.vpc.vpc_id
-#   service_name        = "com.amazonaws.${var.aws_region}.elasticloadbalancing"
-#   vpc_endpoint_type   = "Interface"
-#   private_dns_enabled = true
-#   subnet_ids          = module.vpc.private_subnets
-#   security_group_ids  = [module.vpce_sg.security_group_id]
+resource "aws_vpc_endpoint" "elasticloadbalancing" {
+  vpc_id              = module.vpc.vpc_id
+  service_name        = "com.amazonaws.${var.aws_region}.elasticloadbalancing"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  subnet_ids          = module.vpc.private_subnets
+  security_group_ids  = [module.vpce_sg.security_group_id]
 
-#   tags = { Name = "todo-vpce-elb" }
-# }
+  tags = { Name = "todo-vpce-elb" }
+}
 
-# resource "aws_vpc_endpoint" "acm" {
-#   vpc_id              = module.vpc.vpc_id
-#   service_name        = "com.amazonaws.${var.aws_region}.acm"
-#   vpc_endpoint_type   = "Interface"
-#   private_dns_enabled = true
-#   subnet_ids          = module.vpc.private_subnets
-#   security_group_ids  = [module.vpce_sg.security_group_id]
+resource "aws_vpc_endpoint" "acm" {
+  vpc_id              = module.vpc.vpc_id
+  service_name        = "com.amazonaws.${var.aws_region}.acm"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  subnet_ids          = module.vpc.private_subnets
+  security_group_ids  = [module.vpce_sg.security_group_id]
 
-#   tags = { Name = "todo-vpce-acm" }
-# }
+  tags = { Name = "todo-vpce-acm" }
+}
