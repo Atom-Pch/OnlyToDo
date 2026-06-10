70fec6b9aef983fc8a6fb7d042951af4287b413c
AtomP <112673913+Atom-Pch@users.noreply.github.com>
2026-06-11 00:10:36 +0700

    feat: enhance testing setup with Playwright and TypeScript configuration
    - Added new test scripts in package.json for running Playwright tests in different modes.
    - Updated playwright.config.ts to support multiple browser projects (Chromium, Firefox, WebKit) and improved server configuration.
    - Expanded tsconfig.json to include additional files and directories for TypeScript compilation.

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,7 +1,31 @@
-import { defineConfig } from '@playwright/test';
+import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
-	webServer: { command: 'npm run build && npm run preview', port: 4173 },
+	webServer: {
+		command: 'npm run build && npm run preview',
+		url: 'http://localhost:4173',
+		reuseExistingServer: !process.env.CI
+	},
 	testDir: './tests',
-	testMatch: '**/*.e2e.{ts,js}'
+	testMatch: '**/*.e2e.{ts,js}',
+	projects: [
+		{
+			name: 'chromium',
+			use: { ...devices['Desktop Chrome'] }
+		},
+		{
+			name: 'firefox',
+			use: { ...devices['Desktop Firefox'] }
+		},
+		{
+			name: 'webkit',
+			use: { ...devices['Desktop Safari'] }
+		}
+	],
+	use: {
+		// launchOptions: {
+		// 	slowMo: 1000
+		// },
+		baseURL: 'http://localhost:4173'
+	}
 });
